67a8faca7edaa3556c838354e17ca18451c19326
dmiroshnikov <dmmatrix222@gmail.com>
2016-06-18 13:24:30 +0300

    merege lib

--- core/components/minishop2/custom/payment/fondy.class.php
+++ core/components/minishop2/custom/payment/fondy.class.php
@@ -12,7 +12,7 @@ define('SUCCESS_URL', 'http://minishop/');
 
 
 
-require_once dirname(dirname(dirname(__FILE__))) . '/custom/payment/fondyLib.php';
+
 
 if (!class_exists('msPaymentInterface')) {
 	require_once dirname(dirname(dirname(__FILE__))) . '/model/minishop2/mspaymenthandler.class.php';
@@ -98,4 +98,45 @@ class Fondy extends msPaymentHandler implements msPaymentInterface {
 		die('ERR: ' . $text);
 	}
 
+}
+class FondyForm
+{
+    const RESPONCE_SUCCESS = 'success';
+    const RESPONCE_FAIL = 'failure';
+    const ORDER_SEPARATOR = '#';
+    const SIGNATURE_SEPARATOR = '|';
+    const ORDER_APPROVED = 'approved';
+    const ORDER_DECLINED = 'declined';
+    public static function getSignature($data, $password, $encoded = true)
+    {
+        $data = array_filter($data, function($var) {
+            return $var !== '' && $var !== null;
+        });
+        ksort($data);
+        $str = $password;
+        foreach ($data as $k => $v) {
+            $str .= FondyForm::SIGNATURE_SEPARATOR . $v;
+        }
+        if ($encoded) {
+            return sha1($str);
+        } else {
+            return $str;
+        }
+    }
+    public static function isPaymentValid($oplataSettings, $response)
+    {
+        if ($oplataSettings['merchant_id'] != $response['merchant_id']) {
+            return 'An error has occurred during payment. Merchant data is incorrect.';
+        }
+        if ($response['order_status'] == FondyForm::ORDER_DECLINED) {
+            Mage::throwException('An error has occurred during payment. Order is declined.');
+        }
+		$responseSignature = $response['signature'];
+        unset($response['response_signature_string']);
+		unset($response['signature']);
+		if (FondyForm::getSignature($response, $oplataSettings['secret_key']) != $responseSignature) {
+            return 'An error has occurred during payment. Signature is not valid.';
+        }
+        return true;
+    }
 }
